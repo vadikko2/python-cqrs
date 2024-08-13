a952f3b660c6ef0925121d98809dc285122b6271
Вадим Козыревский <v.kozyrevskiy@timeweb.ru>
2024-08-13 18:01:35 +0300

    Update __init__ file

--- src/cqrs/__init__.py
+++ src/cqrs/__init__.py
@@ -2,7 +2,9 @@ from cqrs.events.event import DomainEvent, ECSTEvent, NotificationEvent
 from cqrs.events.event_emitter import EventEmitter
 from cqrs.events.event_handler import EventHandler
 from cqrs.mediator import EventMediator, RequestMediator
+from cqrs.outbox.producer import SQlAlchemyKafkaEventProducer
 from cqrs.outbox.protocol import EventProducer, Outbox
+from cqrs.outbox.sqlalchemy import SqlAlchemyOutbox
 from cqrs.requests.request import Request
 from cqrs.requests.request_handler import RequestHandler
 
@@ -17,5 +19,7 @@ __all__ = (
     "RequestHandler",
     "Request",
     "Outbox",
+    "SqlAlchemyOutbox",
     "EventProducer",
+    "SQlAlchemyKafkaEventProducer",
 )
