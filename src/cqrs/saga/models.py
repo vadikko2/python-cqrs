69e0c9b773c08042596fa56aec18ddaf59c036ad
Vadim Kozyrevskii <vadikko2@mail.ru>
2026-01-27 18:56:56 +0300

    [Bugfix] Add saga context dataclass wizard supporitng (#45)
    * Add dataclass_wizard supporting into SagaContext
    
    * Fix some problems
    
    * Fix some problems
    
    ---------
    
    Co-authored-by: Вадим Козыревский <v.kozyrevskiy@timeweb.ru>

--- src/cqrs/saga/models.py
+++ src/cqrs/saga/models.py
@@ -1,5 +1,6 @@
 import dataclasses
 import typing
+from dataclass_wizard import asdict, fromdict
 
 # Type variable for from_dict classmethod return type
 _T = typing.TypeVar("_T", bound="SagaContext")
@@ -29,7 +30,7 @@ class SagaContext:
         Returns:
             Dictionary representation of the context.
         """
-        return dataclasses.asdict(self)
+        return asdict(self)
 
     @classmethod
     def from_dict(cls: type[_T], data: dict[str, typing.Any]) -> _T:
@@ -42,11 +43,11 @@ class SagaContext:
         Returns:
             Instance of the context class.
         """
-        # Get field names from dataclass
-        field_names = {f.name for f in dataclasses.fields(cls)}
-        # Filter data to only include known fields
-        filtered_data = {k: v for k, v in data.items() if k in field_names}
-        return cls(**filtered_data)
+        # # Get field names from dataclass
+        # field_names = {f.name for f in dataclasses.fields(cls)}
+        # # Filter data to only include known fields
+        # filtered_data = {k: v for k, v in data.items() if k in field_names}
+        return fromdict(cls, data)
 
     def model_dump(self) -> dict[str, typing.Any]:
         """
