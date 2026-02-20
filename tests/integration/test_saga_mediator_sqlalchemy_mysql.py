1e5a0bb6b63c86a84b753c17ced5c3d10f155219
Вадим Козыревский <v.kozyrevskiy@timeweb.ru>
2026-02-20 14:13:39 +0300

    fix after review

--- tests/integration/test_saga_mediator_sqlalchemy_mysql.py
+++ tests/integration/test_saga_mediator_sqlalchemy_mysql.py
@@ -1,19 +1,21 @@
 """Integration tests for SagaMediator with SqlAlchemySagaStorage (MySQL)."""
 
 import uuid
+from unittest import mock
 
 import pytest
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 
 import cqrs
+from cqrs import events, SagaMap
 from cqrs.saga.storage.enums import SagaStatus
 from cqrs.saga.storage.sqlalchemy import SqlAlchemySagaStorage
 
 from tests.integration.test_saga_mediator_memory import (
-    FailingOrderSaga,
+    _TestContainer, FailingOrderSaga,
     InventoryReservedEventHandler,
     OrderContext,
-    OrderShippedEventHandler,
+    OrderSaga, OrderShippedEventHandler,
     PaymentProcessedEventHandler,
     ProcessPaymentResponse,
     ProcessPaymentStep,
